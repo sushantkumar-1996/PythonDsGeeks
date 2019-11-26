8499d4ac5f35058194ff7558dbda01cd5fbd8b68
sushantkumar-1996 <sushant641996@gmail.com>
2019-11-26 16:29:26 +0530

    New Code

--- LinkedList_insertion.py
+++ LinkedList_insertion.py
@@ -40,5 +40,52 @@ def append(self, new_data):
 """Program to Implement Insertion of Node At all 3 positions"""
 
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertInBeginning(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def InsertAfter(self, prev_node, new_data):
+        if prev_node is None:
+            print("First node in linked list")
+            return
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def InsertAtEnd(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def PrintList(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.InsertAtEnd(6)
+    llist.insertInBeginning(5)
+    llist.insertInBeginning(10)
+    llist.insertInBeginning(11)
+    llist.insertInBeginning(12)
+    llist.InsertAfter(llist.head.next.next, 8)
+    llist.PrintList()
